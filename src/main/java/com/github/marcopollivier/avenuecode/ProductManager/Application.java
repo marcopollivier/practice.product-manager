1cd657070a6debdb1cafd5b35408d9c5e70047c1
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-01-18 20:33:05 -0200

    Adiciona estrutura basica do projeto

---
+++ src/main/java/com/github/marcopollivier/avenuecode/ProductManager/Application.java
@@ -0,0 +1,12 @@
+package com.github.marcopollivier.avenuecode.ProductManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
