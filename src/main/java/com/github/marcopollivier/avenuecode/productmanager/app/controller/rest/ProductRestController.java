b33c37cba64b83b8432036402aebff79d026857d
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-01-22 04:09:18 -0200

    Refatora codigo nao utilizado

--- src/main/java/com/github/marcopollivier/avenuecode/productmanager/app/controller/rest/ProductRestController.java
+++ src/main/java/com/github/marcopollivier/avenuecode/productmanager/app/controller/rest/ProductRestController.java
@@ -8,7 +8,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
